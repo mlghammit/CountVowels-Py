eae9aa7db8ab8c3eab86da77b3bd6444e4b060ad
mlghammit <58157953+mlghammit@users.noreply.github.com>
2024-04-18 22:56:33 -0400

    Add files via upload

---
+++ Lab.py
@@ -0,0 +1,15 @@
+# def countVowels(word): #Spacing
+#     numVowels = 0 #change numvowels = 0
+#     for letter in word: ###intention, change string to word
+#         if letter in "AUIOEaeiou": #Needs comma,
+#             numVowels+=1 
+#     return numVowels
+# print(countVowels("AEIOu"))#It is correct counts only lower case vowels
+
+# def factorial(n):
+#     result = n
+#     for i in range(n-1, 0, -1):
+#         result *= i
+#     return result
+
+# print(factorial(5))
